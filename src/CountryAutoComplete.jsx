c526fc0a4247c9106ac44ff367184ff4cdb57093
fionaDawn <fiona@browze.com>
2021-11-24 23:17:46 +0800

    first session with Areum

---
+++ src/CountryAutoComplete.jsx
@@ -0,0 +1,60 @@
+
+import './AutoComplete.css'
+import countries from './data/countries.json'
+import React from 'react';
+
+const CountryAutoComplete = () => {
+    const [inputState, setInputState] = React.useState("");
+    const [suggestions, setSuggestions] = React.useState([]);
+    const [selectedIndex, setSelectedIndex] = React.useState(0)
+    const [showSuggestions, setShowSuggestionList] = React.useState(false)
+
+    const updateInput = (e) => {
+        setInputState(e.target.value)
+        setShowSuggestionList(!!e.target.value)
+        setSelectedIndex(0)
+    }
+
+    const selectCountry = (index) => {
+        setShowSuggestionList(false)
+        setInputState(suggestions[index].name)
+    }
+
+    const selectFromSuggestions = (e) => {
+        switch (e.keyCode) {
+            // enter
+            case 13:
+                selectCountry(selectedIndex)
+                break;
+            // arrow up
+            case 38:
+                setSelectedIndex(selectedIndex === 0 ? suggestions.length - 1 : selectedIndex - 1)
+                break;
+            // arrow down
+            case 40:
+                setSelectedIndex(selectedIndex === suggestions.length - 1 ? 0 : selectedIndex + 1)
+                break;
+            default:
+                break;
+        }
+    }
+
+    const onMouseOverEvent = (e) => {
+        if (suggestions[parseInt(e.target.id)]) setSelectedIndex(parseInt(e.target.id))
+    }
+
+    React.useEffect(() => {
+        setSuggestions(inputState ? countries.filter(c => c.name.toLowerCase().startsWith(inputState.toLowerCase())) : [])
+    }, [inputState])
+
+    return <div>
+        <input type="text" value={inputState} onChange={updateInput} onKeyDown={selectFromSuggestions} />
+        {showSuggestions &&
+            <ul>
+                {suggestions.map(({ name }, i) =>
+                    <li key={i} id={i} onClick={() => selectCountry(i)} onMouseOver={onMouseOverEvent} className={selectedIndex === i ? 'active' : 'inactive'}>{name}</li>)}
+            </ul>}
+    </div>
+}
+
+export default CountryAutoComplete;
